f486ed909850dc46495739eb5813a5c671835cb7
Nestor Gutiérrez <nesor2@gmail.com>
2018-09-24 16:12:50 -0400

    Configuracion Sequelize
    - Se agregan sequelize y mysql2.
    - Se modifica el script build para copiar archivos e ignorar otros.
    - Se ejecuta el comando sequelize init.
    - Se modifica el nombre del archivo config.js por config-env.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,5 @@
 import bodyParser from 'body-parser'
-import config from './config/config'
+import config from './config/config-env'
 import express from 'express'
 import http from 'http'
 import morgan from 'morgan'
@@ -7,13 +7,16 @@ import router from './router'
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('port', config.port)
 
-app.use(router)
+app.use('/api', router)
+app.get('*', (req, res) => res.status(405).send({
+  message: 'Ups! this route does not exist',
+}));
 
 const server = http.createServer(app)
 
--- server/src/router/index.js
+++ server/src/router/index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (req, res) => res.status(200).send({
-  message: 'Welcome to this little app',
+  message: 'Welcome to the driver register API!',
 }))
 
 export default router
